6e323a9ea3614079f64c03f85b105d361cee67ec
KiwiKey <75843996+kiwikey@users.noreply.github.com>
2026-07-13 08:46:27 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const hash = fs.readFileSync('public/definitions/hash.json', 'utf8');
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/via/',
   plugins: [
     react(),
     createHtmlPlugin({
